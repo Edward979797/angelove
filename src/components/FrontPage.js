6d8d93d7a7b3f950b25bb113c1a9cc5d3d1dcfbf
Edward979797 <edwardceapcin@gmail.com>
2022-04-20 23:04:33 -0400

    Finished polishing up front page

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -3,22 +3,49 @@ import './FrontPage.css';
 
 export default function FrontPage() {
     return (
-        <div className="front-page">
-            <div className="front--header">
+        <div className="front-page" style={{ backgroundImage: `url(${require("../images/regal.png")}) `}}>
+            <div className="front--welcome">
                 <div className="front--logo">
                     <h1>Ange</h1>
                     <img src={require('../images/angel.png')} />
                     <h1>Love</h1>
                 </div>
-                <ul className="front--menu">
-                    <li>About Us</li>
-                    <li>Contact</li>
-                    <li>FAQ</li>
-                    <li>Testimonials</li>
-                </ul>
+                <div className="front--sublogo">
+                    Custom gifts for heavenly events.
+                </div>
+                <p>Birthdays, baby showers, and more&nbsp;&nbsp;|&nbsp;&nbsp;Shipping to anywhere in North America</p>
+                <div className="front--link">
+                    <button>
+                        Browse our selection
+                    </button>
+                </div>
             </div>
-            <div className="front--body" style={{ backgroundImage: `url(${require("../images/regal.png")}) `}}>
-                <button className="front--button">Browse our gifts</button>
+            <div className="pink-feather">
+                 <div className="feather-wrapper">
+                    <div className="feather-down">
+                        <div className="feather-side">
+                            <img src={require('../images/pink-feather.png')} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="gold-feather">
+                 <div className="feather-wrapper">
+                    <div className="feather-down">
+                        <div className="feather-side">
+                            <img src={require('../images/gold-feather.png')} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="grey-feather">
+                 <div className="feather-wrapper">
+                    <div className="feather-down">
+                        <div className="feather-side">
+                            <img src={require('../images/grey-feather.png')} alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>  
     );
